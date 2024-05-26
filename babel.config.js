fd84f193e5e850e252aa0c898699a2f95ff57b79
mariisabellaz <marilia.isabellaz@gmail.com>
2024-05-26 00:44:35 -0300

    create a path mapping

--- babel.config.js
+++ babel.config.js
@@ -1,3 +1,25 @@
 module.exports = {
   presets: ['module:@react-native/babel-preset'],
+  plugins: [
+    [
+      'module-resolver',
+      {
+        root: ['./src'],
+        alias: {
+          '@mocks': './src/mocks',
+          '@assets': './src/assets',
+          '@components': './src/components',
+          '@screens': './src/screens',
+          '@storage': './src/storage',
+          '@utils': './src/utils',
+          '@services': './src/services',
+          '@hooks': './src/hooks',
+          '@contexts': './src/contexts',
+          '@routes': './src/routes',
+          '@theme': './src/theme',
+        },
+      },
+    ],
+    'react-native-reanimated/plugin',
+  ],
 };
