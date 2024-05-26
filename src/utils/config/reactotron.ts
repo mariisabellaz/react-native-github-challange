7aac0c10023f18d9f4ae0024823566f4c0a7b5f6
mariisabellaz <marilia.isabellaz@gmail.com>
2024-05-26 01:29:13 -0300

    config reactotron

---
+++ src/utils/config/reactotron.ts
@@ -0,0 +1,22 @@
+import {NativeModules} from 'react-native';
+import Reactotron from 'reactotron-react-native';
+
+declare global {
+  interface Console {
+    tron: any;
+  }
+}
+
+if (__DEV__) {
+  const {scriptURL} = NativeModules.SourceCode;
+  const scriptHostname = scriptURL.split('://')[1].split(':')[0];
+  const tron = Reactotron.configure({host: scriptHostname})
+    .useReactNative({
+      networking: {
+        ignoreUrls: /symbolicate/,
+      },
+    })
+    .connect();
+
+  console.tron = tron;
+}
